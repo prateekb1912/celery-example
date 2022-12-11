b7c94399534a94248755962e8e86dbfd309d20ff
prateekb1912 <prateekb1912@gmail.com>
2022-12-11 23:14:10 +0530

    Create a web scraper to be scheduled

--- tasks.py
+++ tasks.py
@@ -1,9 +1,47 @@
 from celery import Celery
-from time import sleep
+import requests
+from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+
 
 app = Celery('tasks', backend='db+sqlite:///db.sqlite3', broker='amqp://')
 
 @app.task
-def reverse(text):
-    sleep(5)
-    return text[::-1]
+def save_data(articles_list):
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')    
+    filename = 'articles-{}.json'.format(timestamp)    
+    
+    with open(filename, mode='w') as outFile:
+        json.dump(articles_list, outFile)
+
+@app.task
+def scrape_rss_news():
+    articles_list = []
+
+    try:
+        r = requests.get("https://news.ycombinator.com/rss")
+        soup = BeautifulSoup(r.content, features='xml')
+
+        articles = soup.findAll('item')
+
+        for article in articles:
+            title = article.find('title').text
+            link = article.find('link').text
+            pubDate = article.find('pubDate').text
+
+            articles_list.append(
+                {
+                    'title': title,
+                    'link': link,
+                    'published': pubDate
+                }
+            )
+
+        return save_data(articles_list)
+
+    except Exception as e:
+        print(f"Job failed: {e}")
+
+
+scrape_rss_news()
